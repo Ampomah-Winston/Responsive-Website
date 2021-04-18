719ef9c67b23ed800f3bc239ce6356f49fcf8bf3
proau <ampomah.winston@amalitech.org>
2021-04-18 05:16:05 -0300

    form.js

--- js/form.js
+++ js/form.js
@@ -5,11 +5,11 @@ const phone = document.getElementById('phone');
 const password = document.getElementById('password');
 const conPassword = document.getElementById('conPassword');
 
-form.addEventListener('submit',(e)=>{
+document.getElementById('form').addEventListener('submit', function() {
 	e.preventDefault();
 
 	checkInputs();
-})
+}); 
 
 function checkInputs () {
 	const usernameVal = username.value.trim();                                 
@@ -19,6 +19,25 @@ function checkInputs () {
 	const conPasswordVal =conPassword.value.trim();
 
 	if(usernameVal === ''){
-		setErrorFor(username, "Empty")
+		setErrorFor(username, "Username is empty, fill.")
+	}else{
+		setSuccessFor(username) 
 	}
+
+	if(emailVal === ''){
+		setErrorFor(email, "Email is empty, fill.")
+	}else{
+		setSuccessFor(email) 
+	}
+}
+
+function setErrorFor(input, message){
+	const formControl = input.parentElement;
+	const small = formControl.querySelector('small') ;
+
+	// add error message inside smal
+	small.innerText = message;
+
+	//add error class
+	formControl.className ='form-control error';
 }
